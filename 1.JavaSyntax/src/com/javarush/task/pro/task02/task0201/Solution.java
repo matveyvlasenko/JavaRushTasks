f7165b776a23cd332e128339edee0a6d775a4112
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-03-10 11:06:53 +0100

    add solution for Java Syntax task 0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+       int intValue = 1;
+       int numericValue =2;
+       int size = 3;
+       int number;
+
+
+    }
+}
