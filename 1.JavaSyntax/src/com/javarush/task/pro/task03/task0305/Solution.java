d009ef1b98260a75359bd7337e1adf5ce26f64a9
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-03-13 18:51:37 +0100

    add solution for Java Syntax task 0305

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -9,22 +9,17 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-       int number1 = scanner.nextInt();
-       int number2 = scanner.nextInt();
-       int number3 = scanner.nextInt();
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
 
-       if (number1==number2) {
-           if (number1 == number3) {
-               System.out.println(number1 + " "+number2 + " " +number3);
-           } else {
-               System.out.println(number1 + " "+ number2);
-           }
-       }
-           else if (number1==number3) {
-               System.out.println(number1 + " "+number3);
-           }
-           else if (number2==number3){
-           System.out.println(number2+" "+number3);
-           }
-       }
+        if (number1 == number2 && number1 == number3) {
+            System.out.println(number1 + " " + number2 + " " + number3);
+        } else if (number1 == number3) {
+            System.out.println(number1 + " " + number3);
+        } else if (number2 == number3) {
+            System.out.println(number2 + " " + number3);
+        } else if (number1 == number2) {
+            System.out.println(number1 + " " + number2);
+        }
     }
