d46c1d6737e78728ccbfdf834a6f9ad6736266f8
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-03-30 09:24:26 +0200

    Java rush 0408

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0408/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task04.task0408;
+
+import java.util.Scanner;
+
+/* 
+Максимум из введенных чисел
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+    }
+}
