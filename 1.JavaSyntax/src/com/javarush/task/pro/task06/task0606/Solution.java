16bc3358f8247b3e323e0b454637f5dc1fb27888
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-04-10 20:13:39 +0200

    Java rush 0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(int n){};
+    public static void universalMethod(int nw, String b){ };
+    public static void universalMethod(String ne){ };
+    public static void universalMethod(int a, int b, String n){ };
+    public static void universalMethod(String nv, int nb){ };
+    public static void universalMethod(int m, int v, int zc){ };
+
+    public static void universalMethod(int r, String nr, String vc){ };
+    public static void universalMethod(String c, String cccc){ };
+    public static void universalMethod(String cert, String ert, String ertty){ };
+
+    }
+
+
