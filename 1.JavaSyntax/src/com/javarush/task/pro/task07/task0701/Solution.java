f9a1b7b2bdc1ae0fc1707decdfd1cd07072d6743
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-04-12 10:33:49 +0200

    Java rush 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.pro.task07.task0701;
+
+
+
+public class Solution {
+   byte byte1;
+   short short1;
+   int int1;
+   long long1;
+   float float1;
+   double double1;
+   boolean bool1;
+   char char1;
+}
