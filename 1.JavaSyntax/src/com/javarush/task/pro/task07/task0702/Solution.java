fb2732cd3941249780d875742c50a145962d5427
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-04-12 10:55:04 +0200

    Java rush 0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte byte1 = Byte.MAX_VALUE;
+    short short1 = Short.MAX_VALUE;
+    int int1 = Integer.MAX_VALUE;
+    long long1 = Long.MAX_VALUE;
+    float float1 = Float.MAX_VALUE;
+    double double1 = Double.MAX_VALUE;
+    boolean bool1 = Boolean.TRUE;
+    char char1 = Character.MAX_VALUE;
+}
+
