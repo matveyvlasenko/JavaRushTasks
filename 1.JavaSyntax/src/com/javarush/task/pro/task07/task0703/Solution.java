6c4c40c8ec3c36a30273b0cd82a74312f6086f8f
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-04-12 10:55:15 +0200

    Java rush 0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte byte1 = Byte.MIN_VALUE;
+    short short1 = Short.MIN_VALUE;
+    int int1 = Integer.MIN_VALUE;
+    long long1 = Long.MIN_VALUE;
+    float float1 = Float.MIN_VALUE;
+    double double1 = Double.MIN_VALUE;
+    boolean bool1 = Boolean.TRUE;
+    char char1 = Character.MIN_VALUE;
+}
