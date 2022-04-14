3e97004c56895c9959f09e71047e8d1d1c6f14de
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-04-14 23:12:43 +0200

    Java rush 0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0704/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0704;
+
+/* 
+Экспонентная запись числа
+*/
+
+public class Solution {
+    double earthDiameter = 12.742E+3;
+    double lightSpeed = 2.99792458E+6;
+    double uraniumAtomicMass = 2.380289E+2;
+    double averageBeeWeight = 8.5E+3;
+    double javaDeveloperSalary = 1E+3;
+}
