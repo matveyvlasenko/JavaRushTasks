04a512f09a1b18f13139692be925d1a8ab0b636e
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-05-01 12:02:01 +0200

    Java rush 0814

--- 1.JavaSyntax/src/com/javarush/task/pro/task08/task0814/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0814/Solution.java
@@ -7,17 +7,24 @@ package com.javarush.task.pro.task08.task0814;
 public class Solution {
 
     public static int setFlag(int number, int flagPos) {
-        //напишите тут ваш код
-        return 0;
+        //на позицию flagPos поставь 1 вместо 0
+        //для числа number
+//111 | 1000 => 1111
+        return number|(1 << flagPos);
     }
 
     public static int resetFlag(int number, int flagPos) {
-        //напишите тут ваш код
-        return 0;
+//было 0000001111, хотим 0111. 0001111 & 000111 = 000111. 001111>>
+        //0101111>> 0010111
+        //сдвиг на 4 позиции, инвертируем то что получилось
+        //000001 <<100000 +> 011111
+        //0101111 & 0011111 = 0001111
+        return number &~(1 << flagPos);
     }
 
     public static boolean checkFlag(int number, int flagPos) {
         //напишите тут ваш код
-        return false;
+        //(a & (1 << b)) == (1 << b)
+        return (number & (1 << flagPos)) == (1 << flagPos);
     }
 }
