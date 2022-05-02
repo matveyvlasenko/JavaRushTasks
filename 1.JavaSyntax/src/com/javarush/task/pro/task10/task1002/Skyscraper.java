8a52d502df4a46870eaa0af1543de5ac0379a19b
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-05-02 18:42:56 +0200

    Java rush 1002

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1002/Skyscraper.java
@@ -0,0 +1,36 @@
+package com.javarush.task.pro.task10.task1002;
+
+/* 
+Построим новый жилой комплекс JavaRush Towers
+*/
+
+public class Skyscraper {
+    int floorsCount;
+    public static final String SKYSCRAPER_WAS_BUILD = "Небоскреб построен.";
+    public static final String SKYSCRAPER_WAS_BUILD_FLOORS_COUNT = "Небоскреб построен. Количество этажей - ";
+    public static final String SKYSCRAPER_WAS_BUILD_DEVELOPER = "Небоскреб построен. Застройщик - ";
+
+public Skyscraper(){
+
+    System.out.println(SKYSCRAPER_WAS_BUILD);
+}
+
+
+
+
+
+public Skyscraper(int floorsCount){
+    this.floorsCount= floorsCount;
+    System.out.println(SKYSCRAPER_WAS_BUILD_FLOORS_COUNT + floorsCount);
+}
+public Skyscraper(String developer){
+    System.out.println(SKYSCRAPER_WAS_BUILD_DEVELOPER + developer);
+}
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperTower = new Skyscraper(50);
+        Skyscraper skyscraperSkyline = new Skyscraper("JavaRushDevelopment");
+    }
+}
+
