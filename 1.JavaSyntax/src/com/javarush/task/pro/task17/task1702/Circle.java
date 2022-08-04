3133d2bb95c4ed0a7c3e6456855592517452ec1f
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-08-04 12:07:12 +0200

    JavaRush Tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1702/Circle.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task17.task1702;
+
+public class Circle extends Shape {
+
+    Shape shape = new Shape();
+    Circle circle = new Circle();
+    Rectangle rectangle = new Rectangle();
+    Triangle triangle = new Triangle();
+    public void printInfo() {
+        System.out.println("Круг");
+    }
+
+}
