1b7b86a7ff0893244fbd92e36e61a125f13464a7
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-08-17 16:02:16 +0200

    JavaRush Tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1802/AgeComparator.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task18.task1802;
+
+import java.util.Comparator;
+
+/* 
+Сортировка по возрасту
+*/
+
+public class AgeComparator implements Comparator <Student> {
+    public int compare(Student obj1, Student obj2) {
+        return obj2.getAge() - obj1.getAge();
+    }
+}
