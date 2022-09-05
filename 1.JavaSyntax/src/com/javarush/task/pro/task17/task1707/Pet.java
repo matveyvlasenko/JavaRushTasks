23614d22de2fe4c5ad629c79d2fe124fe64194c2
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-09-05 10:10:40 +0200

    JavaRush Tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1707/Pet.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task17.task1707;
+
+public class Pet {
+    String color;
+    public void printInfo(){
+        System.out.println("Я домашний питомец.");
+
+    }
+    public int countAge (int a){
+        return a*a;
+    }
+}
