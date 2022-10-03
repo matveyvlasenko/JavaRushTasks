3dc7cc4b692b7fb4a1a9ea0f1ec0d3201fc272b0
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-10-03 10:10:08 +0200

    JavaRush Tasks

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1529/Plane.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task15.task1529;
+
+public class Plane implements CanFly {
+    public  int passengerNumber;
+    public Plane(int passengerNumber){
+        this.passengerNumber = passengerNumber;
+    }
+    @Override
+    public void fly() {
+
+    }
+}
