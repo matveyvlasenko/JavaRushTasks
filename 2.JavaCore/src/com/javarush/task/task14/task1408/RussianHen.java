216dd83c85c83cc7303ec79c7032313005fd0e88
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2022-10-10 09:23:21 +0200

    JavaRush Tasks

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1408/RussianHen.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task14.task1408;
+
+public class RussianHen extends Hen{
+
+    @Override
+    public int getCountOfEggsPerMonth() {
+        return 10;
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + " Моя страна - " +
+                Country.RUSSIA + ". Я несу " +
+                this.getCountOfEggsPerMonth() +
+                " яиц в месяц.";
+    }
+}
