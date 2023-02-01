2e3c62fc3f892800014b0af711d2b1527b14ac20
Matvey Vlasenko <matvey.vlasenko@gmail.com>
2023-02-01 09:57:42 +0100

    JavaRush Tasks

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+static void print(int in){
+
+}
+static void print (String s){
+
+}
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+print(new Integer (2));
+print(4);
+    }
+
+   static void print (Integer myInteger){}
+    static void print (int myInteger){}
+}
